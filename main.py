1a0a1b47856e50ca09c18afe42444392582f1675
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-10-26 00:48:43 +0300

    Add get_num_characters function and import it in main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_num_words
+from stats import count_num_words, get_num_characters
 
 
 def get_book_text(file_path):
@@ -11,6 +11,8 @@ def main():
     content = get_book_text(file_path)
     number_of_words = count_num_words(content)
     print(f"Found {number_of_words} total words")
+    char_counter = get_num_characters(content)
+    print(char_counter)
 
 main()
 
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
 def count_num_words(text):
     count = text.split()
     return len(count)
+
+
+def get_num_characters(text):
+    text = text.lower()
+    counter = {}
+    for char in text:
+        if (char in counter):
+            counter[char] += 1
+        else:
+            counter[char] = 1
+    return counter
+        
