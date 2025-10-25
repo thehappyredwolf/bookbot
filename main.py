5066c14231df9f9c6237b57dbcdeb8905a5bb34e
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-10-26 01:18:51 +0300

    Add a function that prints the final report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_num_words, get_num_characters
+from stats import count_num_words, get_num_characters, get_a_sorted_list_of_dicts
 
 
 def get_book_text(file_path):
@@ -6,13 +6,28 @@ def get_book_text(file_path):
         content = f.read()
     return content
 
+
+def print_report(file_path, num_of_words, list_of_sorted_dicts):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_of_words} total words")
+    print("--------- Character Count -------")
+    for dict in list_of_sorted_dicts:
+        if (dict['char'].isalpha()):
+            print(f"{dict['char']}: {dict['num']}")
+    print("============= END ===============")
+
+
+                
+
 def main():
     file_path = "books/frankenstein.txt"
     content = get_book_text(file_path)
     number_of_words = count_num_words(content)
-    print(f"Found {number_of_words} total words")
     char_counter = get_num_characters(content)
-    print(char_counter)
+    list_of_sorted_dicts = get_a_sorted_list_of_dicts(char_counter)
+    print_report(file_path, number_of_words, list_of_sorted_dicts)
 
 main()
 
