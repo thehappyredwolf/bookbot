d658ea7ebd242dd4f96ff55e8e977e0948fbd070
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-10-26 01:33:51 +0300

    Get book_path from argument instead of hard-coded values

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_num_words, get_num_characters, get_a_sorted_list_of_dicts
+import sys
 
 
 def get_book_text(file_path):
@@ -22,12 +23,16 @@ def print_report(file_path, num_of_words, list_of_sorted_dicts):
                 
 
 def main():
-    file_path = "books/frankenstein.txt"
-    content = get_book_text(file_path)
-    number_of_words = count_num_words(content)
-    char_counter = get_num_characters(content)
-    list_of_sorted_dicts = get_a_sorted_list_of_dicts(char_counter)
-    print_report(file_path, number_of_words, list_of_sorted_dicts)
+    if (len(sys.argv) != 2):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        file_path = sys.argv[1]
+        content = get_book_text(file_path)
+        number_of_words = count_num_words(content)
+        char_counter = get_num_characters(content)
+        list_of_sorted_dicts = get_a_sorted_list_of_dicts(char_counter)
+        print_report(file_path, number_of_words, list_of_sorted_dicts)
 
 main()
 
