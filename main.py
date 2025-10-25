06f1ec985e8871994944687d54a247523d7946db
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-10-26 00:09:59 +0300

    Import count_num_words function from stats file

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+from stats import count_num_words
+
+
 def get_book_text(file_path):
     with open(file_path) as f:
         content = f.read()
     return content
-
-def count_num_words(text):
-    count = text.split()
-    return len(count)
 
 def main():
     file_path = "books/frankenstein.txt"
