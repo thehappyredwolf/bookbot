022db3b151e6b9fdbd5446763a71afd38b5971e2
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-10-26 01:18:25 +0300

    Add a function that sorts dictionary of letters based on count

--- stats.py
+++ stats.py
@@ -13,3 +13,13 @@ def get_num_characters(text):
             counter[char] = 1
     return counter
         
+
+def sort_on(items):
+    return items["num"]
+
+def get_a_sorted_list_of_dicts(dictionary):
+    list = []
+    for item in dictionary:
+        list.append({"char": item, "num": dictionary[item]})
+    list.sort(reverse=True, key=sort_on)
+    return list
